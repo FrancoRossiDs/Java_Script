ef22fec6e419a8ba6ee99fd08d12b92d23431a8d
FrancoRossiDs <francorosssic@gmail.com>
2023-10-11 10:54:58 -0300

    subo ejercicios de la primera clase

---
+++ operadores.js
@@ -0,0 +1,28 @@
+/*
+    Operadores Matemáticos
+
++   -> Suma
+-   -> Resta
+*   -> Mult
+/   -> Div
+%   -> Modulo-Resto
+**  -> Elevado
+
+    Operadores Lógicos
+
+>   -> Mayor que 
+<   -> Menor que 
+>=  -> Mayor o igual
+<=  -> Menor o igual
+!== 
+!===
+=   -> Asignación de valor de varible
+== -> Compara valores
+=== -> Compara valor y el tipo de dato
+
+    Operadores
+
+&&  -> AND 
+!   -> Negación
+||  -> OR
+*/
