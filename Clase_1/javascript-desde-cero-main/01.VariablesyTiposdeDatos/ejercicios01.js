35503743166239a19a119856f99f39f7a836a283
FrancoRossiDs <francorosssic@gmail.com>
2023-10-18 10:56:25 -0300

    completo ejercicios de la clase 1 y 2

--- Clase_1/javascript-desde-cero-main/01.VariablesyTiposdeDatos/ejercicios01.js
+++ Clase_1/javascript-desde-cero-main/01.VariablesyTiposdeDatos/ejercicios01.js
@@ -22,33 +22,33 @@ const nuevoModulo = 21 % 5 === 1;
 function primerCaracter(string){
     //Por parametros se pasa una cadena de texto. Tienes que devolver el primer carácter!
     //Tu código:👇
-    return string[0]
+    return (string[0])
 }
 
 function ultimoCaracter(string){
-    //Por parámetros se pasa una cadena de texto. Tienes que devolver el primer carácter!
+    //Por parámetros se pasa una cadena de texto. Tienes que devolver el último carácter!
     //Tu código: 👇
-    return string[string.lenght-1]
+    return (string[string.length - 1])
 }
 
 function longitudDeCadena(string){
     //En este ejercicio tendrás que devolver la longitud de la cadena que se pasa por parámetros!
     //tu código: 👇
-    return string.lenght
+    return (string.length)
 }
 
 function enMayusculas(string){
     //Pasado el string por parámetros tendrás que devolver el mismo contenido del string
     //pero todo en mayúsculas
     //tu código: 👇
-    return string.toUpperCase();
+    return (string.toUpperCase());
 }
 
 function enMinusculas(string){
     //Pasado el string por parámetros tendrás que devolver el mismo contenido del string
     //pero todo en minúsculas
     //tu código: 👇
-    return string.toLowerCase();
+    return (string.toLowerCase());
 }
 
 function enlistandoNombres(args){
@@ -57,30 +57,29 @@ function enlistandoNombres(args){
     //Tendrás que buscar un método el cual sirva para separarlos y crear una lista solo con los nombres!
     //Tu código: 👇
     const nombreArrays = args.split(', ');
-    return nombreArrays
+    return (nombreArrays)
 }
 
 function typeOF(params){
     //Por parametros se pasan distintos tipos de datos, con esta funcion tendras que devolver distintos el tipo de dato que es.
     //Tu código:👇
-    return typeOF(params); 
+    return (typeof(params)); 
 }
 
 function repairToString(str){
     //Por parametro se pasará un string, el cual tiene su primera letra en minuscula y la ultima en mayúscula. EJ: "holA"
     //tendras que devolver el string como corresponda -> "Hola"
     //Tu código:👇
-    const firstLetter = str.charAt(0).toUpperCase();
-    const lastLetter = str.charAt(str.length - 1).toLowerCase();
+    const first = str.charAt(0).toUpperCase();
+    const last = str.charAt(str.length - 1).toLowerCase();
     const middlePart = str.slice(1, str.length - 1);
-  
-    return firstLetter + middlePart + lastLetter;
+    return (first + middlePart + last);
 }
 
 function contains(string){
     // Por parametros se pasará un string, tendrás que verificar si la cadena de texto contiene la siguiente sub-cadena -> "JavaScript"
     // Tu código:👇
-    return string.includes("JavaScript");
+    return (string.includes("JavaScript"));
 }
 
 module.exports = {
