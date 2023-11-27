eed6aa31a88747a00622053281be48098a9b6f69
FrancoRossiDs <francorosssic@gmail.com>
2023-11-27 11:16:32 -0300

    Subo clase 12

---
+++ 12 - QuerySelector/Teoria.js
@@ -0,0 +1,16 @@
+/* Selectores de etiqueta
+document.getElementByID("id")
+document.getElementByClass("class")
+document.querySelector("etiqueta/id/class")==> clase=".class"|id="#id"|etiqueta="etiqueta"
+document.querySelectorAll("")==> clase=".class"|id="#id"|etiqueta="etiqueta"
+*/
+
+/* Modificadores de etiqueta
+.innerHtml==> Modifica su contenido y se puede agregar etiquetas o código HTML
+.innerText==> Modifica o agrega su valor en texto
+*/
+
+/* Eventos
+document.querySelector(".botton").addEventListener(*tipo de evento("click"/"blur"/"keyUp"/"keyDown"/"Key", ()=> alert(*accion a realizar))
+*/
+
