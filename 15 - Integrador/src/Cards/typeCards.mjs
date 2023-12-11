a9c452c34410ac31ed1b381f37d92363cc6dfc2b
FrancoRossiDs <francorosssic@gmail.com>
2023-12-10 23:43:34 -0300

    Subo funciones a integrador

--- 15 - Integrador/src/Cards/typeCards.mjs
+++ 15 - Integrador/src/Cards/typeCards.mjs
@@ -1,16 +1,47 @@
 const main = document.querySelector('main');
-const cardsByType = async (type) => {
+import { displayresult } from '../main.mjs';
+
+const cardsByType = async (filters) => {
+    console.log('Filtros aplicados:', filters); // Imprimir los filtros aplicados
     main.innerHTML = '';
     const res = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php`);
     const info = await res.json();
-    const result = info.data.filter(card => card.type.toLowerCase().includes(type.toLowerCase()));
-    result.forEach(card => {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.innerHTML = `<img src="${card.card_images[0].image_url}" alt="${card.name}">`;
+    
+    let result = info.data;
 
-        main.appendChild(cardElement);
-    });
-}
+    // Aplicar más filtros si están definidos
+    if (filters.name) {
+        console.log('Filtrando por nombre:', filters.name); // Imprimir el filtro de nombre
+        result = result.filter(card => card.name.toLowerCase().includes(filters.name));
+    }
+    if(filters.id){
+        console.log('Filtrando por ID:', filters.id); // Imprimir el filtro de ID
+        result = result.filter(card => card.type.toLowerCase().includes(filters.id));
+    }
+    if (filters.attribute) {
+        console.log('Filtrando por atributo:', filters.attribute); // Imprimir el filtro de atributo
+        result = result.filter(card => card.attribute.toLowerCase().includes(filters.attribute.toLowerCase()));
+    }
 
-export { cardsByType };	
+    if (filters.type) {
+        console.log('Filtrando por tipo:', filters.type); // Imprimir el filtro de tipo
+        result = result.filter(card => card.race.toLowerCase()===(filters.type.toLowerCase()));
+    }
+
+    if (filters.monsterClass) {
+        console.log('Filtrando por clase:', filters.monsterClass); // Imprimir el filtro de clase
+        result = result.filter(card => card.type.toLowerCase().includes(filters.monsterClass.toLowerCase()));
+    }
+
+    if (filters.level) {
+        console.log('Filtrando por nivel:', filters.level); // Imprimir el filtro de nivel
+        result = result.filter(card => card.level === parseInt(filters.level) || card.linkval === parseInt(filters.level));
+    }
+
+    
+    // Muestra las cartas filtradas
+    displayresult(result);
+};
+
+
+export { cardsByType };
--- 15 - Integrador/src/main.mjs
+++ 15 - Integrador/src/main.mjs
@@ -1,35 +1,106 @@
+// main.mjs
+
 import { getCharacters } from './Cards/getCards.mjs';
-import { searchCards } from './Cards/searchCards.mjs';
 import { setupPagination } from './Pages/paginacion.mjs';
 import { cardsByType } from './Cards/typeCards.mjs';
-import { atributeCard } from './Cards/cardAtribute.mjs';
-import { typeCard } from './Cards/cardType.mjs';
-import { classCard } from './Cards/cardClass.mjs';
-import { levelCard } from './Cards/levelCards.mjs';
-import { spellCard } from './Cards/spellType.mjs';
-import { trapCard } from './Cards/trapCards.mjs';
+
+const main = document.querySelector('main');
 
 getCharacters();
 
-
+const reset= document.querySelector('#resetButton');
 const monsterFilters = document.getElementById('monsterFilters');
 const spellFilters = document.getElementById('spellFilters');
 const trapFilters = document.getElementById('trapFilters');
-const atributo= document.querySelector('#atributo');
-const typo= document.querySelector('#type');
-const clase= document.querySelector('#clase');
-const nivel= document.querySelector('#nivel');
-const spell= document.querySelector('#spellType');
-const trap= document.querySelector('#trapType');
+const atributo = document.querySelector('#atributo');
+const typo = document.querySelector('#type');
+const clase = document.querySelector('#clase');
+const nivel = document.querySelector('#nivel');
+const spell = document.querySelector('#spellType');
+const trap = document.querySelector('#trapType');
 const tipoSelect = document.querySelector('#tipo');
 const search = document.querySelector('#searchInput');
 const type = document.querySelector('#tipo');
+
+// Inicializa la paginación
+const { initPagination } = setupPagination(getCharacters);
+initPagination();
+
+
+const filters = {
+    id: null,
+    name: null,
+    attribute: null,
+    type: null,
+    monsterClass: null,
+    level: null,
+};
+
+reset.addEventListener('click', () => {
+    getCharacters();
+    monsterFilters.style.display = 'none';
+    spellFilters.style.display = 'none';
+    trapFilters.style.display = 'none';
+    reset.style.display = 'none';
+})
+
+type.addEventListener('change', (e) => {
+    if (e.target.value === "null") {
+        filters.id= null;
+        cardsByType(filters);
+    } else {
+        filters.id= e.target.value;
+        cardsByType(filters);
+}});
+
+tipoSelect.addEventListener('change', () => {
+    const selectedType = tipoSelect.value;
+
+    // Restablecer los subfiltros cuando cambias el tipo principal
+    filters.type = null;
+    filters.spellType = null;  // Restablecer el subfiltro de hechizos
+    filters.trapType = null;   // Restablecer el subfiltro de trampas
+
+    // Oculta todos los filtros
+    monsterFilters.style.display = 'none';
+    spellFilters.style.display = 'none';
+    trapFilters.style.display = 'none';
+    reset.style.display = 'none';
+
+    // Muestra los filtros correspondientes al tipo seleccionado
+    if (selectedType === 'monster') {
+        monsterFilters.style.display = 'block';
+        reset.style.display = 'block';
+    } else if (selectedType === 'spell') {
+        spellFilters.style.display = 'block';
+        reset.style.display = 'block';
+    } else if (selectedType === 'trap') {
+        trapFilters.style.display = 'block';
+        reset.style.display = 'block';
+    }
+
+    // Aplicar los filtros cuando cambias el tipo principal
+    cardsByType(filters);
+});
+
+
+async function displayresult(result) {
+    main.innerHTML = '';
+    result.forEach(card => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.innerHTML = `<img src="${card.card_images[0].image_url}" alt="${card.name}">`;
+        main.appendChild(cardElement); 
+    });
+}
+
 
 search.addEventListener('blur', (e) => {
     if (e.target.value === "") {
         getCharacters();
     } else {
-        searchCards(e.target.value);
+        filters.name = e.target.value;
+        cardsByType(filters);
     }
 });
 
@@ -37,60 +108,78 @@ search.addEventListener('keydown', (e) => {
     if (e.target.value === "") {
         getCharacters();
     } else if (e.key === "Enter") {
-        searchCards(e.target.value);
-    }
-});
-
-// Inicializa la paginación
-const { initPagination } = setupPagination(getCharacters);
-initPagination();
-
-type.addEventListener('change', (e) => {
-    if(e.target.value === "All"){
+        filters.name = e.target.value;
+        cardsByType(filters);
+    } else if (e.key === "Escape") {
+        e.target.value = "";  
         getCharacters();
-    }else{
-        cardsByType(e.target.value);
-    }
-})
-
-tipoSelect.addEventListener('change', () => {
-    const selectedType = tipoSelect.value;
-
-    // Oculta todos los filtros
-    monsterFilters.style.display = 'none';
-    spellFilters.style.display = 'none';
-    trapFilters.style.display = 'none';
-
-    // Muestra los filtros correspondientes al tipo seleccionado
-    if (selectedType === 'monster') {
-        monsterFilters.style.display = 'block';
-    } else if (selectedType === 'spell') {
-        spellFilters.style.display = 'block';
-    } else if (selectedType === 'trap') {
-        trapFilters.style.display = 'block';
     }
 });
 
 atributo.addEventListener('change', (e) => {
-    atributeCard(e.target.value);
-})
+    if (e.target.value === "null") {
+        filters.attribute = null;
+        cardsByType(filters);
+    } else {
+        filters.attribute = e.target.value;
+        cardsByType(filters);
+    }
+});
+
 
 typo.addEventListener('change', (e) => {
-    typeCard(e.target.value);
-})
+    if (e.target.value === "null") {
+        filters.type = null;
+        cardsByType(filters);
+    }else{
+        filters.type = e.target.value;
+        cardsByType(filters);
+    }
+    
+});
 
 clase.addEventListener('change', (e) => {
-    classCard(e.target.value);
-})
+    if (e.target.value === "null") {
+        filters.monsterClass = null;
+        cardsByType(filters);
+    }else{
+        filters.monsterClass = e.target.value;
+        cardsByType(filters);
+    }
+});
 
 nivel.addEventListener('change', (e) => {
-    levelCard(e.target.value);
-})
+    if (e.target.value === "null") {
+        filters.level = null;
+        cardsByType(filters);
+    }else{
+        filters.level = e.target.value;
+        cardsByType(filters);
+    } 
+});
 
 spell.addEventListener('change', (e) => {
-    spellCard(e.target.value);
-})
+    console.log(e.target.value);  // Agrega esta línea para imprimir el valor
+    if(e.target.value === "null"){
+        filters.type = null;
+        cardsByType(filters);
+    } else {
+        filters.type = e.target.value;
+        cardsByType(filters);
+    }
+});
 
 trap.addEventListener('change', (e) => {
-    trapCard(e.target.value);
-})
+    if (e.target.value === "null") {
+        // Restablecer el subfiltro de trampas
+        filters.trapType = null;
+    } else {
+        // Aplicar el subfiltro de trampas
+        filters.trapType = e.target.value;
+    }
+    // Aplicar los filtros
+    cardsByType(filters);
+});
+
+
+export {displayresult}
